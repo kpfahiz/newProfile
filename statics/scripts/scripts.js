3644889c24ee185f0ba4edfd93f628fac0490221
fahiz <fahizkppullara@gmail.com>
2024-02-01 06:56:39 +0530

    Skill - Python Model modified

--- statics/scripts/scripts.js
+++ statics/scripts/scripts.js
@@ -8,27 +8,27 @@ function myFunction() {
   }
 
 // Get the modal
-var modal = document.getElementById("python");
+var modal_python = document.getElementById("python");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btn_python = document.getElementById("pythonBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span_python = document.getElementsByClassName("python_close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+btn_python.onclick = function() {
+  modal_python.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+span_python.onclick = function() {
+  modal_python.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == modal_python) {
+    modal_python.style.display = "none";
   }
 }
